5bd8bd39ac7f1b33c2d1f232fc511b6de17e07f6
Sky Rolnick <skymeson@gmail.com>
2021-10-29 15:48:31 -0400

    Updated with a basic music visualizer.

--- Assets/AudioSpectrum/Visualizer.cs
+++ Assets/AudioSpectrum/Visualizer.cs
@@ -9,9 +9,25 @@ public class Visualizer : MonoBehaviour
     SpectrumBar.BarType barType;
     int barCount;
 
+    AudioSpectrum spectrum; 
+
+
+    private void Start()
+    {
+        spectrum = GetComponent<AudioSpectrum>();
+    }
     void Update()
     {
-        var spectrum = GetComponent<AudioSpectrum>();
+
+        //LinearSpectrumVisualizer();
+        CircularSpectrumVisualizer();
+
+
+    }
+
+
+    void CircularSpectrumVisualizer()
+    {
 
         if (barCount == spectrum.PeakLevels.Length)
         {
@@ -34,7 +50,11 @@ public class Visualizer : MonoBehaviour
         {
             var x = 6.0f * i / barCount - 3.0f + barWidth / 2;
 
-            var bar = Instantiate(barPrefab, Vector3.right * x + 3*Vector3.forward, transform.rotation) as GameObject;
+            var posX = 5.0f * Mathf.Cos(i * 360 / barCount);
+            var posZ = 5.0f * Mathf.Sin(i * 360 / barCount);
+
+            Vector3 position = new Vector3(posX, 0, posZ); 
+            var bar = Instantiate(barPrefab, position + 3 * Vector3.forward, transform.rotation) as GameObject;
 
             bar.GetComponent<SpectrumBar>().index = i;
             bar.GetComponent<SpectrumBar>().barType = barType;
@@ -42,6 +62,40 @@ public class Visualizer : MonoBehaviour
             bar.transform.parent = transform;
             bar.transform.localScale = barScale;
         }
+    }
 
+
+    void LinearSpectrumVisualizer()
+    {
+
+        if (barCount == spectrum.PeakLevels.Length)
+        {
+            return;
+        }
+
+        // Destroy the old bars.
+        foreach (var child in transform)
+        {
+            Destroy((child as Transform).gameObject);
+        }
+
+        // Change the number of bars.
+        barCount = spectrum.PeakLevels.Length;
+        var barWidth = 6.0f / barCount;
+        var barScale = new Vector3(barWidth * 0.9f, 1, -0.75f);
+
+        // Create new bars.
+        for (var i = 0; i < barCount; i++)
+        {
+            var x = 6.0f * i / barCount - 3.0f + barWidth / 2;
+
+            var bar = Instantiate(barPrefab, Vector3.right * x + 3 * Vector3.forward, transform.rotation) as GameObject;
+
+            bar.GetComponent<SpectrumBar>().index = i;
+            bar.GetComponent<SpectrumBar>().barType = barType;
+
+            bar.transform.parent = transform;
+            bar.transform.localScale = barScale;
+        }
     }
 }
