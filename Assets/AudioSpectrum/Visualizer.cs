4c0775a5bcfa4e720cf50f51f54b84ef84b7f53a
Sky Rolnick <skymeson@gmail.com>
2021-11-05 12:33:53 -0400

    Update

--- Assets/AudioSpectrum/Visualizer.cs
+++ Assets/AudioSpectrum/Visualizer.cs
@@ -21,10 +21,62 @@ public class Visualizer : MonoBehaviour
 
         //LinearSpectrumVisualizer();
         CircularSpectrumVisualizer();
-
+        //SquareSpectrumVisualizer(); 
 
     }
 
+
+    void SquareSpectrumVisualizer()
+    {
+
+
+        if (barCount == spectrum.PeakLevels.Length)
+        {
+            return;
+        }
+
+        // Destroy the old bars.
+        foreach (var child in transform)
+        {
+            Destroy((child as Transform).gameObject);
+        }
+
+        // Change the number of bars.
+        barCount = spectrum.PeakLevels.Length;
+        var barWidth = 6.0f / barCount;
+        var barScale = new Vector3(barWidth * 0.9f, 1, -0.75f);
+
+
+        int ii = (int) Mathf.Sqrt(barCount); 
+        // Create new bars.
+        for (var j = 0; j < ii; j++)
+        {
+            for (var i = 0; i < ii; i++)
+            {
+                //var x = 6.0f * i / barCount - 3.0f + barWidth / 2;
+
+                //var posX = 5.0f * Mathf.Cos(i * 360 / barCount);
+                //var posZ = 5.0f * Mathf.Sin(i * 360 / barCount);
+
+
+                var posX = 6.0f * i / ii - 3.0f + barWidth / 2;
+                var posZ = 6.0f * j / ii - 3.0f + barWidth / 2;
+
+
+                Vector3 position = new Vector3(posX, 0, posZ);
+                var bar = Instantiate(barPrefab, position + 3 * Vector3.forward, transform.rotation) as GameObject;
+
+                bar.GetComponent<SpectrumBar>().index = i*ii+j;
+                bar.GetComponent<SpectrumBar>().barType = barType;
+
+                bar.transform.parent = transform;
+                bar.transform.localScale = barScale;
+            }
+        }
+
+
+
+    }
 
     void CircularSpectrumVisualizer()
     {
